3bf7476363034a5b75d71b1d9cd0570de2d49b48
Kevin Lucas <kevinlucas.tec.info@gmail.com>
2026-06-07 15:53:49 -0300

    sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kevin-lucas.github.io",
+  base: "/kevinportfolio",
   vite: {
     plugins: [tailwindcss()],
   },
